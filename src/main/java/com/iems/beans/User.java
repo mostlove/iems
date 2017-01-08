9efcd19d6f47061e088d0b27546a67d119f33155
xqm <xqm@magicbeans.cn>
2017-01-08 17:52:55 +0800

    1

---
+++ src/main/java/com/iems/beans/User.java
@@ -0,0 +1,11 @@
+package com.iems.beans;
+
+/**
+ * Created by Eric Xie on 2017/1/8.
+ */
+public class User {
+
+
+
+
+}
---
+++ src/main/java/com/iems/util/CommonUtil.java
@@ -0,0 +1,10 @@
+package com.iems.util;
+
+/**
+ * Created by Eric Xie on 2017/1/8.
+ */
+public class CommonUtil {
+
+
+
+}
